67758deac7374dd978c8bdb89f1f63d6fdb5af3a
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-05-30 23:32:54 +0600

    Delete Duplicate Email

--- LeetCode/181_Employes_earn_more_than_boss.sql
+++ LeetCode/181_Employes_earn_more_than_boss.sql
@@ -3,3 +3,8 @@
 SELECT a.Name AS 'Employee' FROM Employee AS a, Employee AS b WHERE a.ManagerId = b.Id AND a.Salary > b.Salary ;
 
 SELECT a.Name AS Employee FROM Employee AS a JOIN Employee AS b ON a.ManagerId = b.Id AND a.Salary > b.Salary ;
+
+
+WITH tmp AS (SELECT MIN(Id) FROM Person GROUP BY Email)
+
+DELETE FROM Person WHERE Id NOT IN (SELECT * FROM tmp)
